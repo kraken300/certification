0c47eccc4e154abacf0e4dfbbf7a597a04149b92
kraken300 <shivamsingh113355@gmail.com>
2024-12-19 20:59:07 +0530

    Add datatypes size

---
+++ java_porgrams/module_1/structure_of_java/DataTypes.java
@@ -0,0 +1,46 @@
+class DataTypes{
+	public static void main(String[] args){
+		System.out.println("Range of data types:");
+
+		// byte datatype range
+		System.out.println("For byte:");
+		System.out.println("MIN : "+Byte.MIN_VALUE);
+		System.out.println("MAX: "+Byte.MAX_VALUE);
+		System.out.println("======================");
+
+		// short datatype range
+		System.out.println("For short:");
+		System.out.println("MIN : "+Short.MIN_VALUE);
+		System.out.println("MAX: "+Short.MAX_VALUE);
+		System.out.println("======================");
+
+		// int datatype range
+		System.out.println("For int:");
+		System.out.println("MIN : "+Integer.MIN_VALUE);
+		System.out.println("MAX: "+Integer.MAX_VALUE);
+
+		// long datatype range
+		System.out.println("For long:");
+		System.out.println("MIN : "+Long.MIN_VALUE);
+		System.out.println("MAX: "+Long.MAX_VALUE);
+		System.out.println("======================");
+
+		// float datatype range
+		System.out.println("For float:");
+		System.out.println("MIN : "+Float.MIN_VALUE);
+		System.out.println("MAX: "+Float.MAX_VALUE);
+		System.out.println("======================");
+
+		// double datatype range
+		System.out.println("For double:");
+		System.out.println("MIN : "+Double.MIN_VALUE);
+		System.out.println("MAX: "+Double.MAX_VALUE);
+		System.out.println("======================");
+
+		// double datatype range
+		System.out.println("For char:");
+		System.out.println("MIN : "+Character.MIN_VALUE);
+		System.out.println("MAX: "+Character.MAX_VALUE);
+		System.out.println("======================");
+	}
+}

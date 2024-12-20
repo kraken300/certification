f12fc73d0fe20fbe1e633a2b35e9e215a52cd06e
kraken300 <shivamsingh113355@gmail.com>
2024-12-20 21:07:43 +0530

    Add LocalVariables1

---
+++ java_porgrams/module_1/Variables/LocalVariables1.java
@@ -0,0 +1,16 @@
+class LocalVariables1{
+	public static void main(String[] args){
+		int a = 10;
+		System.out.println(a);
+
+		{
+			int b = 20;
+			System.out.println(b);
+		}
+
+		{
+			int c = 30;
+			System.out.println(c);
+		}
+	}
+}

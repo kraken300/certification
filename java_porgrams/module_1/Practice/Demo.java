a04bd0b8aceae023c6eaa278990845d9695a6a07
kraken300 <shivamsingh113355@gmail.com>
2024-12-23 20:42:27 +0530

    Escape sequence program

---
+++ java_porgrams/module_1/Practice/Demo.java
@@ -0,0 +1,12 @@
+class Demo{
+	public static void main(String[] args){
+		System.out.println("System.out.println(\"Hello ! World\");");
+	}
+}
+
+// \" To add double quotes
+// \' To add Single quote
+// \n To add New Line
+// \t To add Tab space
+// \b To add Back space(Single backspace = one character is removed)
+// \r To bring the cursor to the beginning of the printed output

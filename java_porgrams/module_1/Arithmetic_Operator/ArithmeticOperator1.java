8cb4846f3e102949916c1c69e5b30a580b6be5a1
kraken300 <shivamsingh113355@gmail.com>
2024-12-23 20:50:06 +0530

    Add ArithmeticOperator1 java file

---
+++ java_porgrams/module_1/Arithmetic_Operator/ArithmeticOperator1.java
@@ -0,0 +1,15 @@
+public class ArithmeticOperator1{
+    public static void main(String[] args){
+        System.out.println(10+20);
+
+        byte b1 = 10;
+        byte b2 = 20;
+        // byte result = b1+b2;
+        // java:7: error: incompatible types: possible lossy conversion from int to byte
+        // byte result = b1+b2;
+        //                 ^
+        // 1 error
+        byte res = 30+10;
+        System.out.println(res);
+    }
+}

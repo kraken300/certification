e19c746b29f718e96349c4ab4e8b92c401e991a1
kraken300 <shivamsingh113355@gmail.com>
2024-12-24 21:32:16 +0530

    Add Relational Operators file

---
+++ java_porgrams/module_1/Operators/RelationalOperators1.java
@@ -0,0 +1,10 @@
+public class RelationalOperators1{
+	public static void main(String[] args) {
+		System.out.println(10>11);	//false
+		System.out.println(10<'5');	//true
+		System.out.println(10>=11); //false
+		System.out.println(10<='5'); //true
+		System.out.println(10==11); //false
+		System.out.println(10!=11); //true
+	}
+}

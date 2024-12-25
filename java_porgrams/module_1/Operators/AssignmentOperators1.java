8cb7bd046f9c4d903dae9d2f597310e534b09e71
kraken300 <shivamsingh113355@gmail.com>
2024-12-25 20:01:42 +0530

    Add Assignment Operators files

---
+++ java_porgrams/module_1/Operators/AssignmentOperators1.java
@@ -0,0 +1,13 @@
+public class AssignmentOperators1{
+	public static void main(String[] args){
+		int a = 5;
+		int b = 10;
+		int res = a+b;
+		res += 10;
+		System.out.println(res);
+
+		int c = 10;
+		c *= 10;
+		System.out.println(c);
+	}
+}

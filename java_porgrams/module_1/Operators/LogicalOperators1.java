42a44f196dcc6739270dbc137af0f4770793acfb
kraken300 <shivamsingh113355@gmail.com>
2024-12-25 20:00:58 +0530

    Add Logical Operators files

---
+++ java_porgrams/module_1/Operators/LogicalOperators1.java
@@ -0,0 +1,11 @@
+public class LogicalOperators1{
+	public static void main(String[] args){
+		System.out.println(10>5 && 20<10);
+
+		System.out.println(10>20 && 5>1);
+
+		System.out.println(10>5 && 15>10 && 1==0 && 1!=0);
+
+		System.out.println((5>6 && 9<8 && 1==0) && (10>9 && 1!=1) && (1>1 && 2>2));
+	}
+}

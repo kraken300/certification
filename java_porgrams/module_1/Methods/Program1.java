69491e327bdf03289d074833420e94399782ab06
kraken300 <shivamsingh113355@gmail.com>
2025-01-02 18:36:42 +0530

    Add first program in Methods

---
+++ java_porgrams/module_1/Methods/Program1.java
@@ -0,0 +1,11 @@
+public class Program1{
+	public static void main(String[] args){
+		int result = add(10, 20);
+		System.out.println(result);
+	}
+
+	public static int add(int a, int b){
+		int sum = a + b;
+		return sum;
+	}
+}

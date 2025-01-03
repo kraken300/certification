95992044fa5be449573d98fac9074462a8cd3679
kraken300 <shivamsingh113355@gmail.com>
2025-01-03 20:32:37 +0530

    Parametrized method file

--- java_porgrams/module_1/Methods/Program1.java
+++ java_porgrams/module_1/Methods/Program1.java
@@ -2,6 +2,17 @@ public class Program1{
 	public static void main(String[] args){
 		int result = add(10, 20);
 		System.out.println(result);
+
+		System.out.println("Result is : "+add(20,20));
+
+		// System.out.println("Result is : "+add());
+		// Program1.java:8: error: method add in class Program1 cannot be applied to given types;
+        //         System.out.println("Result is : "+add());
+        //                                           ^
+  		// required: int,int
+  		// found:    no arguments
+  		// reason: actual and formal argument lists differ in length
+		// 1 error
 	}
 
 	public static int add(int a, int b){

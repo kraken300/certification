69ae708c335a9eb8595b78ff3c598616c9522599
kraken300 <shivamsingh113355@gmail.com>
2025-01-17 19:27:10 +0530

    Add ConstructorOverloading example program

---
+++ java_porgrams/MODULE_2/Constructor/ConstructorOverloading1.java
@@ -0,0 +1,17 @@
+public class ConstructorOverloading1 {
+    int i;
+    int j;
+
+    public ConstructorOverloading1(){
+
+    }
+
+    public ConstructorOverloading1(int i){
+        this.i = i;
+    }
+
+    public ConstructorOverloading1(int i, int j){
+        this.i = i;
+        this.j = j;
+    }
+}

c9b3f48b56ce75b1bca67ba2002f755d0d4eef26
kraken300 <shivamsingh113355@gmail.com>
2025-02-04 22:58:44 +0530

    Add Composition (HAS-A Relationship Type) program

---
+++ java_porgrams/MODULE_2/Relationships/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    int i = 10;
+    Test t;
+
+    public class Test {
+
+        int j = 20;
+    }
+}
---
+++ java_porgrams/MODULE_2/Relationships/DemoDriver.java
@@ -0,0 +1,8 @@
+public class DemoDriver {
+    public static void main(String[] args) {
+        // Test t1 = new Test();
+
+        Demo d1 = new Demo();
+        d1.t = d1.new Test();
+    }
+}

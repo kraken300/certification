3712213f6de6c2a732a99176c1dcd718787575ec
kraken300 <shivamsingh113355@gmail.com>
2025-02-14 20:56:42 +0530

    Add non-primitive typecasting program

---
+++ java_porgrams/MODULE_2/Relationships/IS-A/Typecasting/PersonStudentDriver.java
@@ -0,0 +1,8 @@
+public class PersonStudentDriver {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        Student s1 = new Student();
+
+        Person p2 = s1;
+    }
+}

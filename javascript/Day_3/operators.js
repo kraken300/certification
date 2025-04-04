24dedd4958494c4c0f445372c451410a06d872d3
kraken300 <shivamsingh113355@gmail.com>
2025-04-04 19:35:10 +0530

    Add tokens of javascript(identifiers, keywords, literals, operators))

---
+++ javascript/Day_3/operators.js
@@ -0,0 +1,40 @@
+console.log(10 + 20);
+console.log(300 - 40);
+console.log(2 * 10);
+console.log(5 ** 2);
+console.log(10 * 2);
+console.log(10 ** 2);
+
+console.log(10 / 2);
+console.log(55 / 10);
+
+console.log(105 % 10);
+console.log(100 % 10);
+
+console.log("js" == "js");
+console.log(10 == "10");
+console.log(10 === "10");
+
+console.log(200 > 300);
+console.log(300 < 700);
+
+console.log(10 >= 10);
+console.log(30 <= 30);
+console.log(10 != 100);
+
+var a = 10;
+// a = a + 30;
+a += 30;
+// a -= 5;
+// a *= 2;
+// a /= 2;
+console.log(a);
+
+let x = 40;
+// console.log(x++);
+// console.log(++x);
+// console.log(x);
+
+console.log(--x);
+console.log(x--);
+console.log(x);
---
+++ javascript/Day_3/script.js
@@ -0,0 +1,25 @@
+// var 1a = 70; // !An identifier or keyword cannot immediately follow a numeric literal
+
+var a1 = 100;
+console.log(a1);
+
+let $ = 90;
+console.log($);
+
+var _ = 10000;
+console.log(_);
+
+// let for = "js"; // !Error
+
+var userName = "JOHN";
+console.log(userName);
+
+function getName() {
+
+}
+
+class EmployeeDetails {
+
+}
+
+

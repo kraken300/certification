b381f4c91ed4456f83e41807bd407df4f58a84bf
kraken300 <shivamsingh113355@gmail.com>
2025-07-23 11:45:05 +0530

    Add spring profiles and spring actuator in spring boot

--- advanced_java/spring/StudentAppSpingBoot/src/main/java/com/sb/StudentAppSpingBootApplication.java
+++ advanced_java/spring/StudentAppSpingBoot/src/main/java/com/sb/StudentAppSpingBootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@Profile(value = "dev")
 public class StudentAppSpingBootApplication {
 
 	public static void main(String[] args) {
